bf33882f5df1e513fd9c34a2c5b15c483ea3be44
Peter Muiruri <petemuiruri08@gmail.com>
2019-09-13 19:25:49 +0300

    Add files via upload

---
+++ reports.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Smart Bins</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+
+    <div class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="interface1.php">Smart Bins</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="interface1.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="interface2.php">Menu</a>
+      <!-- <a class="nav-item nav-link" href="#">Pricing</a> -->
+
+    </div>
+  </div>
+</nav>
+
+<h1>My Bin Reports </h1>
+
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Month</th>
+      <th scope="col">Kgs</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>January</td>
+      <td>60</td>
+
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>February</td>
+      <td>30</td>
+
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>March</td>
+      <td>100</td>
+
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>April</td>
+      <td>55</td>
+
+    </tr><tr>
+      <th scope="row">5</th>
+      <td>May</td>
+      <td>25</td>
+
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>June</td>
+      <td>99</td>
+
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>July</td>
+      <td>96</td>
+
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>August</td>
+      <td>122</td>
+
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>September</td>
+      <td>64</td>
+
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>October</td>
+      <td>85</td>
+
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>November</td>
+      <td>186</td>
+
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>December</td>
+      <td>205</td>
+
+    </tr>
+  </tbody>
+</table>
+
+
+
+</div
+
+  </body>
+</html>
---
+++ status.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Smart Bins</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+
+    <div class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="interface1.php">Smart Bins</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="interface1.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="interface2.php">Menu</a>
+      <!-- <a class="nav-item nav-link" href="#">Pricing</a> -->
+
+    </div>
+  </div>
+</nav>
+
+<h1>My Bin </h1>
+
+<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">70%</div>
+</div>
+
+<div class="container">
+  <p>Current: 20 Kgs</p>
+  <p>Remaining: 10 Kgs</p>
+  <p>Last emptied: 3/09/2019</p>
+
+</div>
+
+
+</div
+
+  </body>
+</html>
